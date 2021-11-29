e3a341763eb5cebc98396e93c6a5363550421b1b
RodrigoSendinoSanz <78493406+RodrigoSendinoSanz@users.noreply.github.com>
2021-11-29 22:52:10 +0100

    update

--- Rodrigo/JS/js/ajax.js
+++ Rodrigo/JS/js/ajax.js
@@ -1,19 +1,24 @@
-const URL_DESTINO = "http://localhost:5500/"; 
+const URL_DESTINO = "http://localhost:5500/";
 const DATOS = "pizza.json";
 
-function miFuncionAsync(str) {
+function FuncionAsync(str) {
   let pagina = new XMLHttpRequest();
 
   pagina.onreadystatechange = function () {
     if (this.readyState == 4) {
       if (this.status == 200) {
-        if (str === "mostrar") {
-          mostrarDatos(this.responseText);
-        } else if (str === "procesar") {
-          sacarPedido(this.responseText);
+        if (str === "borrar") {
+          RefresInfo(this.responseText);
+        } else if (str === "precio") {
+          verPrecioPedido(this.responseText);
         }
       } else {
-        alert("ERROR");
+        swal({
+          title: "Error!",
+          text: "El nombre debe tener una letra en mayuscula y no se aceptan numeros!",
+          icon: "error",
+          button: "Ok",
+        });
       }
     }
   };
@@ -21,27 +26,21 @@ function miFuncionAsync(str) {
   pagina.send(null);
 }
 
-
-function sacarPedido(jsonDoc) {
+function verPrecioPedido(jsonDoc) {
   var json = JSON.parse(jsonDoc);
   var listaTAM = json.PIZZA.TAM;
   var listaING = json.PIZZA.ING;
 
-
   let size = document.getElementsByName("tamaño");
-  let tamano = ""; 
+  let tamano = "";
   for (let i = 0; i < size.length; i++) {
-   
-    console.log(size[i].value);
     if (size[i].checked) {
-    
-      tamano = size[i].value; 
+      tamano = size[i].value;
       break;
     }
   }
   var precioTam = 0;
   for (let i of listaTAM) {
-    console.log(i.TAMAÑO +" "+ i.PRECIO);
     if (tamano === i.TAMAÑO) {
       precioTam = i.PRECIO;
     }
@@ -61,86 +60,66 @@ function sacarPedido(jsonDoc) {
       }
     }
   }
-  console.log(precioTam);
-  console.log(precioIngre);
-  
-  var precioFinal = precioTam + precioIngre; 
-  let tuPedido = `Tu pizza tamaño ${tamano} con ${addIngre} tiene un precio total de ${precioFinal}€.`;
-  let mensaje = document.createTextNode(tuPedido); 
-  parrafo.appendChild(mensaje); 
-  console.log(precioFinal);
+
+  var precioTotal = precioTam + precioIngre;
+  let tuPedido = `Tu pizza tamaño ${tamano} con ${addIngre} tiene un precio total de ${precioTotal}€.`;
+  let mensaje = document.createTextNode(tuPedido);
+  parrafo.appendChild(mensaje);
 }
 
-
-
-
-
-
-function mostrarDatos(jsonDoc) {
-  var json = JSON.parse(jsonDoc); 
+function RefresInfo(jsonDoc) {
+  var json = JSON.parse(jsonDoc);
 
   var listaTAM = json.PIZZA.TAM;
   var listaING = json.PIZZA.ING;
 
-
-  var inputRad = [];
-  var inputTxtRad = [];
+  var inputTam = [];
+  var inputConTam = [];
 
   for (let i of listaTAM) {
-    inputRad[i] = document.createElement("input");
-    inputRad[i].type = "radio";
-    inputRad[i].className = "inputRadio";
-    inputRad[i].name = "tamaño";
-    inputRad[i].id = `tamaño_${i.TAMAÑO}`;
-    inputRad[i].value = i.TAMAÑO;
+    inputTam[i] = document.createElement("input");
+    inputTam[i].type = "radio";
+    inputTam[i].className = "inputTam";
+    inputTam[i].name = "tamaño";
+    inputTam[i].id = `tamaño_${i.TAMAÑO}`;
+    inputTam[i].value = i.TAMAÑO;
 
-    inputTxtRad[i] = document.createTextNode(i.TAMAÑO);
+    inputConTam[i] = document.createTextNode(i.TAMAÑO);
 
-    tamapizza.appendChild(inputRad[i]);
-    tamapizza.appendChild(inputTxtRad[i]);
+    tamapizza.appendChild(inputTam[i]);
+    tamapizza.appendChild(inputConTam[i]);
   }
 
-  var inputCheck = [];
-  var inputTxtChecks = [];
+  var inputIng = [];
+  var inputTxtIng = [];
 
   for (let i of listaING) {
-    inputCheck[i] = document.createElement("input");
-    inputCheck[i].type = "checkbox";
-    inputCheck[i].className = "inputCheck";
-    inputCheck[i].name = "ingredientes";
-    inputCheck[i].id = i.INGREDIENTE;
-    inputCheck[i].value = i.INGREDIENTE;
+    inputIng[i] = document.createElement("input");
+    inputIng[i].type = "checkbox";
+    inputIng[i].className = "inputIng";
+    inputIng[i].name = "ingredientes";
+    inputIng[i].id = i.INGREDIENTE;
+    inputIng[i].value = i.INGREDIENTE;
 
-    inputTxtChecks[i] = document.createTextNode(i.INGREDIENTE);
+    inputTxtIng[i] = document.createTextNode(i.INGREDIENTE);
 
-    ingrepizza.appendChild(inputCheck[i]);
-    ingrepizza.appendChild(inputTxtChecks[i]);
+    ingrepizza.appendChild(inputIng[i]);
+    ingrepizza.appendChild(inputTxtIng[i]);
   }
+  console.log("Referencias refrescadas");
 }
-
-function animationpizza() {
-  document.getElementsByClassName("inputRadio").onclick = cambiarTamano();
-  document.getElementsByClassName("inputCheck").onclick = eligeingrdientes(); 
-}
-
-
 
 window.onload = function () {
-
-  animationpizza();
-
-refrescar.addEventListener("click", function () {
-  let mostrar = "mostrar";
-  tamapizza.innerHTML = "";
-  ingrepizza.innerHTML = "";
-  miFuncionAsync(mostrar);
-});
-
-  sacarPrecio.addEventListener("click", function () {
-    let procesar = "procesar";
+  verPrecio.addEventListener("click", function () {
+    let precio = "precio";
     parrafo.innerHTML = "";
-    miFuncionAsync(procesar);
+    FuncionAsync(precio);
+  });
+
+  refrescar.addEventListener("click", function () {
+    let borrar = "borrar";
+    tamapizza.innerHTML = "";
+    ingrepizza.innerHTML = "";
+    FuncionAsync(borrar);
   });
 };
-
-
